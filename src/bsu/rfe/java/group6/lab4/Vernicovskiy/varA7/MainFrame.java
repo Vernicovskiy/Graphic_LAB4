49d23b51c1779b992a6369d585f75750082a4f54
Vernicovskiy <victor02vernicovskiy@gmail.com>
2021-11-27 01:57:25 +0300

    Created MainFrame

--- src/bsu/rfe/java/group6/lab4/Vernicovskiy/varA7/MainFrame.java
+++ src/bsu/rfe/java/group6/lab4/Vernicovskiy/varA7/MainFrame.java
@@ -1,4 +1,24 @@
 package bsu.rfe.java.group6.lab4.Vernicovskiy.varA7;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 600;
+    public  MainFrame(){
+        super("Построение графиков функции на основе заранее подготовленных файлов");
+        setSize(WIDTH,HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit(); // отцентровка
+        setLocation((kit.getScreenSize().width - WIDTH) / 2,
+                (kit.getScreenSize().height - HEIGHT) / 2);
+
+    }
+     public static void main(String args[]){
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+     }
 }
