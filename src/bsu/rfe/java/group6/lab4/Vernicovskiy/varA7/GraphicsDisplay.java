980de1e94e8dd75848e3308db6fdd81a4b3cb504
Vernicovskiy <victor02vernicovskiy@gmail.com>
2021-11-27 18:31:30 +0300

    Added chart content properties

--- src/bsu/rfe/java/group6/lab4/Vernicovskiy/varA7/GraphicsDisplay.java
+++ src/bsu/rfe/java/group6/lab4/Vernicovskiy/varA7/GraphicsDisplay.java
@@ -1,4 +1,26 @@
 package bsu.rfe.java.group6.lab4.Vernicovskiy.varA7;
 
-public class GraphicsDisplay {
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsDisplay extends JPanel {
+    // Basic Stroke класс для задания типа линий
+    // класс Font задание шрифта отображения надписей
+    private  BasicStroke graphicsStroke; // Тип пера для черчения линии графика
+    private  BasicStroke axisStroke; // Тип пера для черчения осей координат
+    private  BasicStroke markerStroke; // Тип пера для черчения контуров маркеров
+    private Font axisFont; //Шрифт для вывода подписей осей координат
+
+    public GraphicsDisplay() {
+        setBackground(Color.WHITE);
+
+        graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_ROUND, 10.0f, new float[]{3, 1, 1, 1, 1, 1, 2, 1, 2, 1}, 0.0f);
+        axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+        markerStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0F, null, 0.0f);
+
+        axisFont = new Font("Serif", Font.BOLD, 36);
+    }
 }
